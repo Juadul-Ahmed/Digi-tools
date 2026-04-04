4b589e7f782a5408be5dc96812afc68b09af9c74
juadul <parallaxhub@gmail.com>
2026-04-04 19:43:12 +0800

    api and card section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import BannerSection from "./Components/BannerSection"
 import Count from "./Components/Count"
 import NavBar from "./Components/NavBar"
+import Products from "./Components/Products"
+
+
+const getProducts = async () => {
+  const res = await fetch('/products.json')
+  return res.json()
+}
+
+const productPromise = getProducts() 
+
 
 function App() {
   
@@ -10,6 +20,7 @@ function App() {
    <NavBar/>
    <BannerSection/>
    <Count/>
+   <Products productPromise={productPromise}/>
     </>
   )
 }
